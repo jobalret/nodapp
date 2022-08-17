562f794d364373d33afe5bf88c6f5a7a54f31834
DARK SIDE <87608232+jobalret@users.noreply.github.com>
2022-08-17 13:06:27 +0530

    Add files via upload

---
+++ destrucing.js
@@ -0,0 +1,34 @@
+let [first,,,last]=[1,2,3,4,5,7,8]
+// var [a,b,c]=[1,2,3]
+// var {x,y}={'x':a,'y':b}
+
+
+// console.log(x)
+// var a=arr[0]
+// var b=arr[1]
+console.log(last)
+
+
+ function Alima(a,b,stream){
+    this.name=a;
+;    this.city=b;
+this.course=stream
+ }
+ var usmani=  new Alima('alema','pune','bca')
+ console.log(usmani)
+
+ var naseeb=new Alima('budu','haryana','bcom')
+ console.log(naseeb)
+
+
+ var sai=new Alima('sai','haryana','bc')
+ console.log(sai)
+ function uss(a){
+    return  a+2
+ }
+
+ var main =new Alima(uss,'functin',1234)
+ console.log(main)
+
+
+ console.log(main.course)
